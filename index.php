b4f2fcb9a4662296cd9d2ac149b97142fa9d3da2
omarghonaim <omarghonaim25@gmail.com>
2021-12-27 14:00:20 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+get_header(); ?>
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+	<div id="primary" <?php astra_primary_class(); ?>>
+		<?php 
+		astra_primary_content_top();
+		
+		astra_content_loop();
+
+		astra_pagination();
+
+		astra_primary_content_bottom(); 
+		?>
+	</div><!-- #primary -->
+<?php 
+if ( astra_page_layout() == 'right-sidebar' ) :
+
+	get_sidebar();
+
+endif;
+
+get_footer();
